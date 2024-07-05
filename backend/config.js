2521469c699d6dfd72c3a9b9e831ba94cdf57076
Selenayk <selenaykeskinm@gmail.com>
2024-07-05 17:59:14 +0300

    Created Book model with mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,8 @@
+import assert from 'assert';
 import dotenv from 'dotenv';
 dotenv.config();
+
+assert(process.env.MONGODB_URL?.length > 0, "MONGODB_URL env var must be set");
 
 export const PORT = 2000;
 export const mongoDBUrl = process.env.MONGODB_URL;
