a707928bad1359c067ba9247d587212ad12df4c5
Selenayk <selenaykeskinm@gmail.com>
2024-07-05 17:35:55 +0300

    Added MongoDB connection with Mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBUrl } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -8,6 +9,14 @@ app.get('/', (req, res) => {
   return res.status(200).send('Welcome to MERN project.');
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
-});
+mongoose
+  .connect(mongoDBUrl)
+  .then(() => {
+    console.log('Connected to database.');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const PORT = 2000;
+export const mongoDBUrl = process.env.MONGODB_URL;
