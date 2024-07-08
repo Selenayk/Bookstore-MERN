ae8b95a81ba9bac85b235fc757f2efed5dc93db4
Selenayk <selenaykeskinm@gmail.com>
2024-07-09 00:28:50 +0300

    Created basic structure for the home page.

--- backend/app.js
+++ backend/app.js
@@ -8,13 +8,15 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-  })
-);
+app.use(cors());
+
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to MERN project.');
