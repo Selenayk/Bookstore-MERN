5141739c494388b97c6850e27a150eb29e3ee886
Selenayk <selenaykeskinm@gmail.com>
2024-07-08 22:36:26 +0300

    Set up CORS

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,19 @@ import express from 'express';
 import { PORT, mongoDBUrl } from './config.js';
 import mongoose from 'mongoose';
 import bookRouter from './routes/book.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to MERN project.');
