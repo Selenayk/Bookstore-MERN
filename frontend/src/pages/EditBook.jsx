7a3c06acaf95f658c48a444e9aeedcda799954f7
Selenayk <selenaykeskinm@gmail.com>
2024-07-09 22:53:33 +0300

    Added EditBook component for editing books

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,7 +1,91 @@
-const EditBook = () => {
-  return (
-    <div>EditBook</div>
-  )
-}
+import { useState, useEffect } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
-export default EditBook
+const EditBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:2000/api/books/${id}`)
+      .then((res) => {
+        setAuthor(res.data.author);
+        setPublishYear(res.data.publishYear);
+        setTitle(res.data.title);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert('An error happened. Please Chack console');
+        console.log(err);
+      });
+  }, [id]);
+  const handleEditBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .put(`http://localhost:2000/api/books/${id}`, data)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert('An error happened. Please Chack console');
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4 font-bold">Edit Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className="flex flex-col border-2 border-gray-500 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500 font-bold">Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500 font-bold">Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2  w-full "
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500 font-bold">Publish Year</label>
+          <input
+            type="number"
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2  w-full "
+          />
+        </div>
+        <button className="p-3 bg-gray-400 m-9 font-bold text-xl" onClick={handleEditBook}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EditBook;
--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -19,46 +19,52 @@ const ShowBook = () => {
       })
       .catch((err) => {
         console.log(err);
-        setLoading(false)
+        setLoading(false);
       });
-  }, []);
+  }, [id]); //Added 'id' to the dependency array
 
   return (
-    <div className='p-4'>
+    <div className="p-4">
       <BackButton />
-      <h1 className='text-3xl my-4 font-bold'>Show Book</h1>
+      <h1 className="text-3xl my-4 font-bold">Show Book</h1>
       {loading ? (
         <Spinner />
       ) : (
-        <div className='flex flex-col border-2 border-gray-500 rounded-xl w-fit p-4'>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500 font-bold'>Id</span>
+        <div className="flex flex-col border-2 border-gray-500 rounded-xl w-fit p-4">
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500 font-bold">Id</span>
             <span>{book._id}</span>
           </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500 font-bold'>Title</span>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500 font-bold">Title</span>
             <span>{book.title}</span>
           </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500 font-bold'>Author</span>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500 font-bold">Author</span>
             <span>{book.author}</span>
           </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500 font-bold'>Publish Year</span>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500 font-bold">
+              Publish Year
+            </span>
             <span>{book.publishYear}</span>
           </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500 font-bold'>Create Time</span>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500 font-bold">
+              Create Time
+            </span>
             <span>{new Date(book.createdAt).toString()}</span>
           </div>
-          <div className='my-4'>
-            <span className='text-xl mr-4 text-gray-500 font-bold'>Last Update Time</span>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500 font-bold">
+              Last Update Time
+            </span>
             <span>{new Date(book.updatedAt).toString()}</span>
-          </div>  
+          </div>
         </div>
       )}
     </div>
-  )
+  );
 };
 
 export default ShowBook;
