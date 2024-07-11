2e4139cb03911415bda3c5f342e1bd132d9aaa33
Selenayk <selenaykeskinm@gmail.com>
2024-07-11 19:40:24 +0300

    Make Book Card a single component

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -1,44 +1,45 @@
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import { MdOutlineDelete } from 'react-icons/md';
 
 const BooksTable = ({ books }) => {
   return (
     <table className="w-full border-separate border-spacing-2">
       <thead>
         <tr>
-          <th className="bg-gray-300 border border-slate-600 rounded-md font-bold">No</th>
-          <th className="bg-gray-300 border border-slate-600 rounded-md font-bold">
+          <th className="bg-gray-300 border border-slate-600 rounded-md font-bold p-2">No</th>
+          <th className="bg-gray-300 border border-slate-600 rounded-md font-bold p-2">
             Title
           </th>
-          <th className="bg-gray-300 border border-slate-600 rounded-md max-md:hidden font-bold">
+          <th className="bg-gray-300 border border-slate-600 rounded-md font-bold p-2 hidden md:table-cell">
             Author
           </th>
-          <th className="bg-gray-300 border border-slate-600 rounded-md max-md:hidden font-bold">
+          <th className="bg-gray-300 border border-slate-600 rounded-md font-bold p-2 hidden md:table-cell">
             Publish Year
           </th>
-          <th className="bg-gray-300 border border-slate-600 rounded-md font-bold">
+          <th className="bg-gray-300 border border-slate-600 rounded-md font-bold p-2">
             Operations
           </th>
         </tr>
       </thead>
       <tbody>
         {books.map((book, index) => (
-          <tr key={book._id} className="h-8">
-            <td className="bg-gray-300 border border-slate-700 rounded-md text-center font-bold">
+          <tr key={book._id} className="h-12">
+            <td className="bg-gray-300 border border-slate-700 rounded-md text-center font-bold p-2">
               {index + 1}
             </td>
-            <td className="border border-slate-700 rounded-md text-center">
+            <td className="border border-slate-700 rounded-md text-center p-2">
               {book.title}
             </td>
-            <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+            <td className="border border-slate-700 rounded-md text-center p-2 hidden md:table-cell">
               {book.author}
             </td>
-            <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+            <td className="border border-slate-700 rounded-md text-center p-2 hidden md:table-cell">
               {book.publishYear}
             </td>
-            <td className="border border-slate-700 rounded-md text-center">
+            <td className="border border-slate-700 rounded-md text-center p-2">
               <div className="flex justify-center gap-x-4">
                 <Link to={`/books/details/${book._id}`}>
                   <BsInfoCircle className="text-2xl text-green-700" />
@@ -56,6 +57,17 @@ const BooksTable = ({ books }) => {
       </tbody>
     </table>
   );
+};
+
+BooksTable.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+      publishYear: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
 
 export default BooksTable;
